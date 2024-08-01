ebaea7f603d3a3f8288027d8c68f49a145f65465
Kaulina <iv_kaulina@mail.ru>
2024-08-01 18:05:54 +0700

    inition commit

---
+++ src/test/java/ru/netology/springboots_conditional/SpringBootsConditionalApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.springboots_conditional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootsConditionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
